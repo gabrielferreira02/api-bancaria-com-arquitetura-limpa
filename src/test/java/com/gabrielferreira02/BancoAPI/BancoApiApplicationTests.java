6cf12e802761dd740f63817254fd79f0b25d74b9
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-12-19 20:20:50 -0300

    add base project structure

---
+++ src/test/java/com/gabrielferreira02/BancoAPI/BancoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.BancoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
