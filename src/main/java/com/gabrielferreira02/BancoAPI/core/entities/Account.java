361e59c07573e0e88e0f792e5df81e6c8e66336e
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-01-12 16:36:02 -0300

    feat: add constructor to mapping entities and register update time

--- src/main/java/com/gabrielferreira02/BancoAPI/core/entities/Account.java
+++ src/main/java/com/gabrielferreira02/BancoAPI/core/entities/Account.java
@@ -29,17 +29,41 @@ public class Account {
         this.password = password;
         this.balance = new BigDecimal(0);
         this.cpf = cpf;
+        this.id = UUID.randomUUID();
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+//   constructor to mapping jpa entity
+    public Account(UUID id,
+                   String firstName,
+                   String lastName,
+                   String cpf,
+                   String password,
+                   BigDecimal balance,
+                   LocalDateTime createdAt,
+                   LocalDateTime updatedAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.password = password;
+        this.balance = balance;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public void debit(BigDecimal amount) {
         if(amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Valor da transferência precisa ser maior que 0");
         if(this.balance.subtract(amount).compareTo(BigDecimal.ZERO) < 0) throw new IllegalArgumentException("Valor da transferência maior que o saldo");
         this.balance = this.balance.subtract(amount);
+        this.updatedAt = LocalDateTime.now();
     }
 
     public void credit(BigDecimal amount) {
         if(amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Valor da transferência precisa ser maior que 0");
         this.balance = this.balance.add(amount);
+        this.updatedAt = LocalDateTime.now();
     }
 
     public UUID getId() {
@@ -53,6 +77,7 @@ public class Account {
 
         this.firstName = firstName;
         this.lastName = lastName;
+        this.updatedAt = LocalDateTime.now();
     }
 
     public String getFirstName() {
@@ -74,6 +99,7 @@ public class Account {
     public void setPassword(String password) {
         if(!password.matches("[0-9]{4}")) throw new IllegalArgumentException("Senha inválida");
         this.password = password;
+        this.updatedAt = LocalDateTime.now();
     }
 
     public BigDecimal getBalance() {
--- src/main/java/com/gabrielferreira02/BancoAPI/core/entities/Transaction.java
+++ src/main/java/com/gabrielferreira02/BancoAPI/core/entities/Transaction.java
@@ -9,9 +9,11 @@ public class Transaction {
     private final BigDecimal amount;
     private final UUID receiver;
     private final UUID sender;
-    private LocalDateTime createdAt;
+    private final LocalDateTime createdAt;
 
-    public Transaction(BigDecimal amount, UUID receiver, UUID sender) {
+    public Transaction(BigDecimal amount,
+                       UUID receiver,
+                       UUID sender) {
         if(amount.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Valor da transferência tem que ser maior que 0");
         if(receiver == null) throw new IllegalArgumentException("Destinatário inválido");
         if(sender == null) throw new IllegalArgumentException("Emissor inválido");
@@ -20,6 +22,21 @@ public class Transaction {
         this.amount = amount;
         this.receiver = receiver;
         this.sender = sender;
+        this.id = UUID.randomUUID();
+        this.createdAt = LocalDateTime.now();
+    }
+
+//    constructor to mapping
+    public Transaction(UUID id,
+                       BigDecimal amount,
+                       UUID receiver,
+                       UUID sender,
+                       LocalDateTime createdAt) {
+        this.id = id;
+        this.amount = amount;
+        this.receiver = receiver;
+        this.sender = sender;
+        this.createdAt = createdAt;
     }
 
     public UUID getId() {
